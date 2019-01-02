4a3234215089df565cbd6650e98f62f069986de7
caizhaoxiong1205 <2489469481qq.com>
2019-01-02 10:41:23 +0800

    11222

--- code/microservice-discovery-eureka/src/main/java/com/itmuch/cloud/EurekaApplication.java
+++ code/microservice-discovery-eureka/src/main/java/com/itmuch/cloud/EurekaApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaApplication {
   public static void main(String[] args) {
 	System.out.println("111111111111");
+	System.out.println("1111111111112222");
     SpringApplication.run(EurekaApplication.class, args);
   }
 }
